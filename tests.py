f590f591bb8dfb9074a9763ba89235321aa1b626
Francesc Alted <faltet@gmail.com>
2018-02-28 18:40:19 +0100

    Printed versions for pycblosc and c-blosc during a test run

--- tests.py
+++ tests.py
@@ -99,5 +99,6 @@ class TestUM(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    print("Running tests for Blosc version:", cblosc.get_version_string())
+    print("Running tests for PyCBlosc {} (C-Blosc {})".format(
+        cblosc.__version__, cblosc.get_version_string()))
     unittest.main()
