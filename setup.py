793d639d985fa106db271c386da034f800807b0b
Francesc Alted <faltet@gmail.com>
2018-02-28 18:30:22 +0100

    Check that we have a reasonable recent C-Blosc library installed

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools_scm import get_version
+from setuptools_scm import get_version as scm_get_version
+from pycblosc import get_version_string as blosc_get_version
 from distutils.core import setup
+import semver
 
+# Check that we have a reasonable recent C-Blosc library installed
+blosc_version = blosc_get_version()
+blosc_version = blosc_version[:blosc_version.rfind('.')]  # workaround for semver compliance
+blosc_version_info = semver.parse_version_info(blosc_version)
+if blosc_version_info.major != 1 or blosc_version_info.minor < 14:
+    raise ValueError("Underlying C-Blosc should be 1.14 or higher")
+print(blosc_version_info)
 
 setup(
     name='pycblosc',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    version=get_version(),
+    version=scm_get_version(),
     py_modules=['pycblosc'],
-    )
+)
